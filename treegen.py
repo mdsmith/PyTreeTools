957ea6b7b1d1b5c9a9f34bf7e159262e3241dde4
Martin Smith <msmith@ucsd.edu>
2013-08-29 21:57:16 -0700

    Iterative versions

--- treegen.py
+++ treegen.py
@@ -24,6 +24,73 @@ def gen_newick(part, number):
                 + ")"
                 + str(number))
 
+def get_unrooted_iteratively(num_taxa):
+    tree = ["(", "N", ");"]
+    covered = 1
+    index = 0
+    while covered < num_taxa:
+        if tree[index] == "N" and (covered == 1 or covered == 2):
+            tree[index] = "N"
+            tree.insert(index + 1, ",")
+            tree.insert(index + 2, "N")
+            covered += 1
+            index += 3
+        elif tree[index] == "N":
+            tree[index] = "("
+            tree.insert(index + 1, "N")
+            tree.insert(index + 2, ",")
+            tree.insert(index + 3, "N")
+            tree.insert(index + 4, ")")
+            tree.insert(index + 5, "I")
+            covered += 1
+            index += 6
+        else:
+            index += 1
+        index = index % len(tree)
+
+    leafI = 1
+    intI = num_taxa + 1
+    for i,c in enumerate(tree):
+        if c == "N":
+            tree[i] = str(leafI)
+            leafI += 1
+        if c == "I":
+            tree[i] = str(intI)
+            intI += 1
+
+    return "".join(tree)
+
+
+def get_rooted_iteratively(num_taxa):
+    tree = ["(", "N", ");"]
+    covered = 1
+    index = 0
+    while covered < num_taxa:
+        if tree[index] == "N":
+            tree[index] = "("
+            tree.insert(index + 1, "N")
+            tree.insert(index + 2, ",")
+            tree.insert(index + 3, "N")
+            tree.insert(index + 4, ")")
+            tree.insert(index + 5, "I")
+            covered += 1
+            index += 6
+        else:
+            index += 1
+        index = index % len(tree)
+
+    leafI = 1
+    intI = num_taxa + 1
+    for i,c in enumerate(tree):
+        if c == "N":
+            tree[i] = str(leafI)
+            leafI += 1
+        if c == "I":
+            tree[i] = str(intI)
+            intI += 1
+
+    return "".join(tree)
+
 def get_unrooted_tree(num_taxa):
     result = gen_unrooted_newick(   [a for a in range(1, int(num_taxa) + 1)],
                                     (2*int(num_taxa))-1,
@@ -69,6 +136,8 @@ if __name__ == "__main__":
                 file=sys.stderr)
         exit(1)
     if type == "rooted":
-        print(get_tree(numBranches))
+        #print(get_tree(numBranches))
+        print(get_rooted_iteratively(int(numBranches)))
     elif type == "unrooted":
-        print(get_unrooted_tree(numBranches))
+        #print(get_unrooted_tree(numBranches))
+        print(get_unrooted_iteratively(int(numBranches)))
